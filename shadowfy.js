90499a1a834654619e033213d4b31c3700a88704
Asuka <suryanshkapil405@gmail.com>
2023-02-27 10:51:39 +0530

    changed to npm

--- shadowfy.js
+++ shadowfy.js
@@ -1,7 +1,5 @@
-const Compiler = require("./compilers/Compiler")
-const ch = new Compiler({
-    src_dir: "./src",
-    out_dir: "./out"
-})
+const Compiler = require('./compilers/Compiler')
 
-ch.convert()
+const Shadowfy = Compiler
+
+module.exports = Shadowfy
